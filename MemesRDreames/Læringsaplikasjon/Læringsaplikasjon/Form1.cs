261e6d6a65d756e4f16134ea454c2df977640d3a
Dodern <jacob.hemstad.gimle@gmail.com>
2017-03-20 09:35:11 +0100

    Pls nigga

--- MemesRDreames/Læringsaplikasjon/Læringsaplikasjon/Form1.cs
+++ MemesRDreames/Læringsaplikasjon/Læringsaplikasjon/Form1.cs
@@ -19,6 +19,84 @@ namespace Læringsaplikasjon
             this.WindowState = FormWindowState.Maximized;
         }
 
+        public static int poeng = 0;
+        ferdigboks form2 = new ferdigboks();
+
+        private void Spill_Ferdig()
+        {
+            if (poeng == 5)
+            {
+                form2.Show();
+            }
+            else if (poeng == 3 || poeng == 4)
+            {
+                form2.Show();
+            }
+            else if (poeng <= 2)
+            {
+                form2.Show();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSjekk_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void memeSjekk_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Bitmap))
+                e.Effect = DragDropEffects.Move;
+        }
+
+        private void memeSjekk_DragDrop(object sender, DragEventArgs e)
+        {
+            e.Data.GetData(DataFormats.FileDrop, false);
+            var bmp = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+            memeSjekk.BackgroundImage = bmp;
+        }
+
+
+        private void bilde1_MouseDown(object sender, MouseEventArgs e)
+        {
+            var img = bilde1.Image;
+            if (img == null) return;
+            if (DoDragDrop(img, DragDropEffects.Move) == DragDropEffects.Move)
+            { }
+        }
+
+        private void bilde2_MouseDown(object sender, MouseEventArgs e)
+        {
+            var img = bilde2.Image;
+            if (img == null) return;
+            if (DoDragDrop(img, DragDropEffects.Move) == DragDropEffects.Move)
+            { }
+        }
+
+        private void bilde3_MouseDown(object sender, MouseEventArgs e)
+        {
+            var img = bilde3.Image;
+            if (img == null) return;
+            if (DoDragDrop(img, DragDropEffects.Move) == DragDropEffects.Move)
+            {
+                bilde3.Image = null;
+            }
+        }
+
+        private void bilde4_MouseDown(object sender, MouseEventArgs e)
+        {
+            var img = bilde4.Image;
+            if (img == null) return;
+            if (DoDragDrop(img, DragDropEffects.Move) == DragDropEffects.Move)
+            {
+                bilde4.Image = null;
+            }
+        }
         /*public void Sjekk_Svar()
         {
             if ()
